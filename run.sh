ec4cea23cfc5dea7230c67727c139960faf34a57
Paolo Venturi <paolo.venturi@diennea.com>
2019-04-19 11:46:39 +0200

    Set Carapace to use db configuration as default #34

--- run.sh
+++ run.sh
@@ -13,7 +13,7 @@ cd carapace-server-1.0
 timeout 22 sh -c 'until nc -z $0 $1; do sleep 1; done' localhost 8001
 
 # apply dynamic configuration
-curl -X POST --data-binary @conf/server.db.dynamic.properties http://localhost:8001/api/config/apply --user admin:admin -H "Content-Type: text/plain"
+curl -X POST --data-binary @conf/server.dynamic.properties http://localhost:8001/api/config/apply --user admin:admin -H "Content-Type: text/plain"
 
 tail -f server.service.log
 
