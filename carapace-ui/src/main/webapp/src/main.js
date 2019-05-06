ef6c0cfcd3f64635529f472698157a956ab18ca0
Matteo Minardi <minardi.matteo@hotmail.it>
2019-05-06 11:36:27 +0200

    Migrating from NPM to Yarn and upgranding dependencies (#67)

--- carapace-ui/src/main/webapp/src/main.js
+++ carapace-ui/src/main/webapp/src/main.js
@@ -2,10 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './app.scss'
-import BootstrapVue from 'bootstrap-vue'
 import { formatTimestamp } from './lib/formatter'
 
-Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
 // Filters
