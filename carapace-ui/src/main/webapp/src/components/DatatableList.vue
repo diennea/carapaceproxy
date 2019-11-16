0896daf4e868efdf1fbb34a5c075bf77a89cdba8
Paolo Venturi <paolo95venturi@gmail.com>
2019-11-16 15:34:45 +0100

    Improved datalisttable with research by cell-string-value

--- carapace-ui/src/main/webapp/src/components/DatatableList.vue
+++ carapace-ui/src/main/webapp/src/components/DatatableList.vue
@@ -1,6 +1,14 @@
 <template>
-    <div>
+    <div class="mt-4">
+        <b-input-group id="filter">
+            <b-form-input type="search" placeholder="Type to Search" v-model="filter"></b-form-input>
+            <b-input-group-append>
+                <b-button :disabled="!filter" @click="filter = ''">Clear</b-button>
+            </b-input-group-append>
+        </b-input-group>
+        <b-form-select id="selector" v-model="perPage" :options="pageOptions" />
         <b-table
+            id="datatable"
             hover
             striped
             responsive
@@ -12,16 +20,27 @@
             :currentPage="currentPage"
             :sort-by="sortBy"
             :sort-compare="sortCompare"
+            :filter="filter"
+            @filtered="onFiltered"
             @row-clicked="rowClicked"
             :class="['tall', { 'row-clickable': rowClicked }]"
         >
             <!-- This allows to forward b-table columns-value customization out of the component -->
-            <template v-for="field of fields" v-slot:[`cell(${field.key})`]="data">
+            <template
+                v-for="field of fields"
+                v-slot:[`cell(${field.key})`]="data"
+            >
                 <slot :name="field.key" v-bind:item="data.item">{{data.value}}</slot>
             </template>
         </b-table>
-        <b-pagination id="pager" :total-rows="totalRows" :per-page="perPage" v-model="currentPage"></b-pagination>
-        <b-form-select id="selector" :options="pageOptions" v-model="perPage" />
+        <b-pagination
+            align="center"
+            class="m-0"
+            pills
+            v-model="currentPage"
+            :total-rows="totalRows"
+            :per-page="perPage"
+        ></b-pagination>
     </div>
 </template>
 
@@ -36,7 +55,7 @@ export default {
     },
     data() {
         return {
-            pageOptions: [10, 25, 50, 250],
+            pageOptions: [10, 25, 50],
             perPage: 10,
             totalRows: this.items.length,
             currentPage: 1,
@@ -48,12 +67,15 @@ export default {
                     return compareTimestamp(a[key], b[key]);
                 }
                 return a[key] < b[key] ? -1 : a[key] > b[key] ? 1 : 0;
-            }
+            },
+            filter: null
         };
     },
-    watch: {
-        items() {
-            this.totalRows = this.items.length;
+    methods: {
+        onFiltered(filteredItems) {
+            // Trigger pagination to update the number of buttons/pages due to filtering
+            this.totalRows = filteredItems.length;
+            this.currentPage = 1;
         }
     }
 };
@@ -65,11 +87,16 @@ export default {
 
 .tall {
     max-height: 80vh;
+    margin-bottom: 0.5rem;
 }
 
-#pager {
+#datatable {
+    margin-top: 0.25rem;
+}
+
+#filter {
     float: left;
-    margin-bottom: 0;
+    width: 50%;
 }
 
 #selector {
