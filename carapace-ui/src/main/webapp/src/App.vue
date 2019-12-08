7c6c61e9ec23048c5cb6bf2038dc5a4e19b8b978
Paolo Venturi <paolo95venturi@gmail.com>
2019-12-08 20:04:51 +0100

    Added Carapace Logo to ui

--- carapace-ui/src/main/webapp/src/App.vue
+++ carapace-ui/src/main/webapp/src/App.vue
@@ -1,78 +1,78 @@
 <template>
     <div id="app" class="d-flex">
         <sidebar
-            :header="'Carapace (' + peerId + ')'"
-            headerCollapsed="CA"
+            logo="assets/logo-full-white.png"
+            logoCollapsed="assets/logo-small.png"
             :elements="[
-        {
-            label: 'Backends',
-            icoName: 'server',
-            href: '/',
-        },
-        {
-            label: 'Directors',
-            icoName: 'crosshairs',
-            href: '/directors',
-        },
-        {
-            label: 'Actions',
-            icoName: 'bolt',
-            href: '/actions',
-        },
-        {
-            label: 'Routes',
-            icoName: 'map-signs',
-            href: '/routes',
-        },
-        {
-            label: 'Listeners',
-            icoName: 'door-open',
-            href: '/listeners',
-        },
-        {
-            label: 'Headers',
-            icoName: 'heading',
-            href: '/headers',
-        },
-        {
-            label: 'Filters',
-            icoName: 'filter',
-            href: '/requestfilters',
-        },
-        {
-            label: 'Certificates',
-            icoName: 'file-signature',
-            href: '/certificates',
-        },
-            {
-            label: 'Cache',
-            icoName: 'archive',
-            href: '/cache',
-        },
-        {
-            label: 'Users',
-            icoName: 'users',
-            href: '/users',
-        },
-        {
-            label: 'Metrics',
-            icoName: 'chart-bar',
-            href: '/metrics',
-        },
-        {
-            label: 'Peers',
-            icoName: 'network-wired',
-            href: '/peers',
-        },
-        {
-            label: 'Configuration',
-            icoName: 'sliders-h',
-            href: '/configuration',
-        },
-
-    ]"
+                {
+                    label: 'Backends',
+                    icoName: 'server',
+                    href: '/',
+                },
+                {
+                    label: 'Directors',
+                    icoName: 'crosshairs',
+                    href: '/directors',
+                },
+                {
+                    label: 'Actions',
+                    icoName: 'bolt',
+                    href: '/actions',
+                },
+                {
+                    label: 'Routes',
+                    icoName: 'map-signs',
+                    href: '/routes',
+                },
+                {
+                    label: 'Listeners',
+                    icoName: 'door-open',
+                    href: '/listeners',
+                },
+                {
+                    label: 'Headers',
+                    icoName: 'heading',
+                    href: '/headers',
+                },
+                {
+                    label: 'Filters',
+                    icoName: 'filter',
+                    href: '/requestfilters',
+                },
+                {
+                    label: 'Certificates',
+                    icoName: 'file-signature',
+                    href: '/certificates',
+                },
+                    {
+                    label: 'Cache',
+                    icoName: 'archive',
+                    href: '/cache',
+                },
+                {
+                    label: 'Users',
+                    icoName: 'users',
+                    href: '/users',
+                },
+                {
+                    label: 'Metrics',
+                    icoName: 'chart-bar',
+                    href: '/metrics',
+                },
+                {
+                    label: 'Peers',
+                    icoName: 'network-wired',
+                    href: '/peers',
+                },
+                {
+                    label: 'Configuration',
+                    icoName: 'sliders-h',
+                    href: '/configuration',
+                }
+            ]"
         />
         <router-view class="main flex-grow-1 p-2" />
+        <span id="peerId">{{peerId}}</span>
     </div>
 </template>
 
@@ -90,10 +90,10 @@ export default {
             peerId: ""
         };
     },
-    created() {        
+    created() {
         doGet("/api/cluster/localpeer", data => {
             this.peerId = data.id;
-            document.title += " (" + this.peerId + ")";
+            document.title = this.peerId;
         });
     }
 };
@@ -104,6 +104,12 @@ export default {
     font-family: "Avenir", Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
+}
+
+#peerId {
+    position: absolute;
+    right: 0.5rem;
+    bottom: 0;
 }
 
 .main {
