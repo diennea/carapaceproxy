c2c2f1d8ccc5e7d7ae09f6b55e88c6fd4d44416a
Paolo Venturi <37150740+pv3nturi@users.noreply.github.com>
2021-01-22 10:21:44 +0100

    Logs for connection lifecycle debug (#237)
    * Logs for connection lifecycle debug
    
    * Carapace logger
    
    * Carapace logger fix
    
    * Connection release on channelReadComplete
    
    * Release when no data read from remote
    
    * revert connection release with no remote data
    
    * Connection release on readCompletedFromRemote
    
    * rollback  Connection release on readCompletedFromRemote
    
    Co-authored-by: Paolo Venturi <paolo.venturi@diennea.com>

--- carapace-server/src/main/java/org/carapaceproxy/server/RuntimeServerConfiguration.java
+++ carapace-server/src/main/java/org/carapaceproxy/server/RuntimeServerConfiguration.java
@@ -42,6 +42,7 @@ import javax.net.ssl.SSLContext;
 import org.carapaceproxy.server.mapper.StandardEndpointMapper;
 import static org.carapaceproxy.server.certificates.DynamicCertificatesManager.DEFAULT_DAYS_BEFORE_RENEWAL;
 import static org.carapaceproxy.server.config.NetworkListenerConfiguration.DEFAULT_SSL_PROTOCOLS;
+import org.carapaceproxy.utils.CarapaceLogger;
 
 /**
  * Configuration
@@ -298,6 +299,9 @@ public class RuntimeServerConfiguration {
         ocspStaplingManagerPeriod = properties.getInt("ocspstaplingmanager.period", 0);
         LOG.info("ocspstaplingmanager.period=" + ocspStaplingManagerPeriod);
 
+        boolean loggingDebugEnabled = properties.getBoolean("logging.debug.enabled", false);
+        CarapaceLogger.setLoggingDebugEnabled(loggingDebugEnabled);
+        LOG.info("logging.debug.enabled=" + loggingDebugEnabled);
     }
 
     private void tryConfigureCertificates(ConfigurationStore properties) throws ConfigurationNotValidException {
---
+++ carapace-server/src/main/java/org/carapaceproxy/utils/CarapaceLogger.java
@@ -0,0 +1,47 @@
+/*
+ * Licensed to Diennea S.r.l. under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. Diennea S.r.l. licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
+package org.carapaceproxy.utils;
+
+import java.util.logging.Level;
+
+/**
+ *
+ * @author paolo.venturi
+ */
+public class CarapaceLogger {
+
+    private static final java.util.logging.Logger LOG = java.util.logging.Logger.getLogger(CarapaceLogger.class.getName());
+
+    private static boolean loggingDebugEnabled = false;
+
+    private CarapaceLogger()  {}
+
+    public static boolean isLoggingDebugEnabled() {
+        return loggingDebugEnabled;
+    }
+
+    public static void setLoggingDebugEnabled(boolean loggingDebugEnabled) {
+        CarapaceLogger.loggingDebugEnabled = loggingDebugEnabled;
+    }
+
+    public static void debug(String s, Object ... o) {
+        LOG.log(loggingDebugEnabled ? Level.INFO : Level.FINE, s, o);
+    }
+}
