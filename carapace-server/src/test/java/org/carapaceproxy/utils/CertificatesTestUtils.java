e2d010d73382b1ac5c4cf06d88fc8405825e6476
Paolo Venturi <paolo.venturi@diennea.com>
2021-03-16 12:19:23 +0100

    Add certificate order already valid management + acme client limbs bump

--- carapace-server/src/test/java/org/carapaceproxy/utils/CertificatesTestUtils.java
+++ carapace-server/src/test/java/org/carapaceproxy/utils/CertificatesTestUtils.java
@@ -1,21 +1,21 @@
 /*
- Licensed to Diennea S.r.l. under one
- or more contributor license agreements. See the NOTICE file
- distributed with this work for additional information
- regarding copyright ownership. Diennea S.r.l. licenses this file
- to you under the Apache License, Version 2.0 (the
- "License"); you may not use this file except in compliance
- with the License.  You may obtain a copy of the License at
-
- http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing,
- software distributed under the License is distributed on an
- "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- KIND, either express or implied.  See the License for the
- specific language governing permissions and limitations
- under the License.
-
+ * Licensed to Diennea S.r.l. under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. Diennea S.r.l. licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
  */
 package org.carapaceproxy.utils;
 
@@ -93,7 +93,7 @@ public class CertificatesTestUtils {
 
         //create end user cert signed by Intermediate CA
         int offset = 1000 * 60 * 60 * 24; // yesterday/tomorrow
-        Date expiringDate = new Date(System.currentTimeMillis() + (expired ? - offset : + offset));
+        Date expiringDate = new Date(System.currentTimeMillis() + (expired ? -offset : +offset));
         builder = new JcaX509v3CertificateBuilder(
                 intermediateCA, //here intermedCA is issuer authority
                 BigInteger.valueOf(new Random().nextInt()),
@@ -110,11 +110,10 @@ public class CertificatesTestUtils {
                 .build(new JcaContentSignerBuilder("SHA256withRSA").setProvider("BC").
                         build(intermedCAKeyPair.getPrivate())));// private key of signing authority , here it is signed by intermedCA
 
-        return new X509Certificate [] {
+        return new X509Certificate[]{
             endUserCert,
             intermediateCA,
-            rootCA,
-        };
+            rootCA,};
     }
 
     public static byte[] generateSampleChainData() throws Exception {
