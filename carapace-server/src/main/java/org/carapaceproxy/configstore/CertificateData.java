c0e53510a89373c0b2d0e662ff58668ef943e312
Niccolò Maltoni <niccolo.maltoni@diennea.com>
2023-01-17 15:48:54 +0100

    fix(server): fix Steam flattening

--- carapace-server/src/main/java/org/carapaceproxy/configstore/CertificateData.java
+++ carapace-server/src/main/java/org/carapaceproxy/configstore/CertificateData.java
@@ -100,7 +100,7 @@ public class CertificateData {
         return Stream
                 .concat(
                     Stream.of(domain),
-                    Optional.ofNullable(subjectAltNames).stream().map(Set::stream).map(String.class::cast)
+                    Optional.ofNullable(subjectAltNames).stream().flatMap(Set::stream)
                 )
                 .collect(Collectors.toUnmodifiableSet());
     }
