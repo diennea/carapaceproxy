bc63d2a97f3540e7e978e3616358f2c1e5c53043
Niccolò Maltoni <niccolo.maltoni@diennea.com>
2024-10-29 16:23:39 +0100

    refactor: delombok EndpointKey

--- carapace-server/src/test/java/org/carapaceproxy/RawClientTest.java
+++ carapace-server/src/test/java/org/carapaceproxy/RawClientTest.java
@@ -215,13 +215,13 @@ public class RawClientTest {
     public void endpointKeyTest() {
         {
             EndpointKey entryPoint = EndpointKey.make("localhost:8080");
-            assertThat(entryPoint.getHost(), is("localhost"));
-            assertThat(entryPoint.getPort(), is(8080));
+            assertThat(entryPoint.host(), is("localhost"));
+            assertThat(entryPoint.port(), is(8080));
         }
         {
             EndpointKey entryPoint = EndpointKey.make("localhost");
-            assertThat(entryPoint.getHost(), is("localhost"));
-            assertThat(entryPoint.getPort(), is(0));
+            assertThat(entryPoint.host(), is("localhost"));
+            assertThat(entryPoint.port(), is(0));
         }
     }
 
